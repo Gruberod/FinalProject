5e6b4d72029884dba80c7747973e4b17edee1e79
Daniela G <daniela.gruberova@merck.com>
2016-09-30 17:09:45 +0200

    user login fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:name])
+    
     if user && user.authenticate(params[:password])
+
       session[:user_id] = user.id
       redirect_to '/recipes'
     else
